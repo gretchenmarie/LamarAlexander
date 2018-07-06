5e1162480e83f90f86bccae8e6c34207c54b578f
gretchenmarie <gretchenmward@gmail.com>
2018-07-06 15:06:58 -0500

    added java selector

---
+++ main.js
@@ -0,0 +1,6 @@
+// selecting the DOM element
+let  target = document.getElementById ("mission");
+// we are inserting content to the DOM
+target.innerHTML += `<div class = "subcomponentfour"> My mission is to serve the People of Tennessee.</div>`;
+
+
